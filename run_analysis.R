d399d9855b1c58782b6556af14a2ccf10408a553
tdwbiz <tdwbiz@gmail.com>
2015-03-19 23:37:58 -0400

    tdwbiz20150319

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+## Getting and Cleaning Data Courde Project
+## Author tdwbiz  - Tom de Witt 2015-0319
+
 ##      create one R script called run_analysis.R that does the following. 
 # 1     Merges the training and the test sets to create one data set.
 # 2     Extracts only the measurements on the mean and standard deviation for each measurement. 
